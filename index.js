28f5e6857775ea21114e3856628f582d3baff9fb
Martha <martha.maina@student.moringaschool.com>
2023-03-30 08:03:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const divide = function() {
+  
+  const divide = (num3, num4) => {
+    let num3 = 2000
+    let num4 = 100
     return 2000 / 100;
   }
   
   const square = (num) => {
-    return num * num;
+    let x = 2
+    return 2 * 2;
   }
   
   const add = (num1, num2) => {
-    return num1 + num2;
+    let a = 3
+    let b = 4
+    return a + b;
   }
-  
